b3e33e4226ae0dace20680192cd56f59c3b211ca
Lis <genia.grishenko@gmail.com>
2026-01-26 11:55:20 +0200

    Finish task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+    
+    const emailData = email.value.trim();
+    const passwordData = password.value.trim();
+    if (emailData === '' || passwordData === '') {
+        alert("All fields must be filled!");
+        return;
+    }
+
+    const formData = { email: emailData, password: passwordData };
+    console.log(formData);
+    event.currentTarget.reset();
+})
 
